add123377b9561d47aea2a202d882dcbed69b7ec
Guns-Akimbo <uzovictor526@gmail.com>
2023-08-03 23:55:20 +0100

    victor commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,23 +29,3 @@ const myApp = () => {
 
 export default myApp
 
-
-
-// "name": "pratice-react",
-// "version": "0.0.0",
-// "dependencies": {
-//   "@reduxjs/toolkit": "^1.9.5",
-//   "alert": "^5.1.4",
-//   "axios": "^1.4.0",
-//   "icons": "^1.0.0",
-//   "persist": "^0.2.7",
-//   "react": "^18.2.0",
-//   "react-dom": "^18.2.0",
-//   "react-icons": "^4.10.1",
-//   "react-redux": "^8.1.1",
-//   "react-router-dom": "^6.14.2",
-//   "redux-persist": "^6.0.0",
-//   "sweet": "^0.1.1",
-//   "sweet-alert": "^2.0.5",
-//   "sweetalert2": "^11.7.20"
-// },
