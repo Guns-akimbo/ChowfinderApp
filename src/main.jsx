f753140ba3d31cc396f0b31cb230ee5d5f52e9b9
Guns-Akimbo <uzovictor526@gmail.com>
2023-08-08 23:44:38 +0100

    newchanges

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import store from "./Store/Store.jsx";
-import { HashRouter as Router} from "react-router-dom";
-let persistor = persistStore(store)
-
-
-
+import { HashRouter as Router } from "react-router-dom";
+let persistor = persistStore(store);
+import Theme from "./Context/Theme.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <Router>
-          <App />
+          <Theme>
+            <App />
+          </Theme>
         </Router>
       </PersistGate>
     </Provider>
