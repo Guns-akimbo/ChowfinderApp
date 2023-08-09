58157f1fff0d3cb7e40f120e37d8dd8ed62464d2
Ochemeta <122886353+Ochemeta@users.noreply.github.com>
2023-08-09 14:03:43 +0100

    verify

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,12 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import store from "./Store/Store.jsx";
 let persistor = persistStore(store)
-
-
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <Router>
-          <App />
+          <App />    
         </Router>
       </PersistGate>
     </Provider>
