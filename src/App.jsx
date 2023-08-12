98cf47d10ae719ffd730050dae45d2e628b2f929
Guns-Akimbo <uzovictor526@gmail.com>
2023-08-12 04:36:43 +0100

    mary reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import './App.css'
-import Home from './Componets/Home/home';
-import Aboutus from './Componets/AboutUs/About Us';
-import Login from './Onboarding/Login';
-import Menu from './Pages/Menu';
-import { Route, Routes } from 'react-router-dom';
-import Verification from "./Onboarding/Verfication/Verification"
-import Detailpage from './Pages/DetailPage';
+import "./App.css";
+import Home from "./Componets/Home/home";
+import Aboutus from "./Componets/AboutUs/About Us";
+import Login from "./Onboarding/Login";
+import Menu from "./Pages/Menu";
+import { Route, Routes } from "react-router-dom";
+import Verification from "./Onboarding/Verfication/Verification";
+import Detailpage from "./Pages/DetailPage";
+import Cart from "./Componets/Cart/Cart";
+import ResetPassword from "./Onboarding/Forgetpassword/ResetPassword";
 
 const myApp = () => {
   return (
-
-
     <div>
-     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/About' element={<Aboutus/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/menu/:restaurantId/*' element={<Menu />}/>
-      <Route path="/verification/:token" element ={<Verification/>}/>
-      {/* <Route path="/detail/:categoryId/:restaurantId" element ={<Detailpage/>}/> */}
-      {/* <Route path="/detail/:categoryId/:restaurantId" element={<Detailpage />} /> */}
-      {/* <Route path="/detail/:categoryId/:restaurantId/:mealId" element={<Detailpage />} /> */}
-      <Route path="/detail/:categoryId/:restaurantId/:mealId" element={<Detailpage />} />    
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<Aboutus />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/menu/:restaurantId/*" element={<Menu />} />
+        <Route path="/verification/:token" element={<Verification />} />
+        <Route
+          path="/detail/:categoryId/:restaurantId/:mealId"
+          element={<Detailpage />}
+        />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/resetpassward/:token" element={<ResetPassword />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-
-export default myApp
-
-
+export default myApp;
